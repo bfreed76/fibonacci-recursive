9ca1b1bf2d2cbf8dd03f9ea87f121d2f5a7f3e71
Brinton <brintonfoyreed@gmail.com>
2021-11-07 07:20:15 -0800

    Finished

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -2,14 +2,15 @@ function fibonacci(n) {
   if (n<2) {
     return n
   }
+
+  return fibonacci(n-1) + fibonacci(n-2)
   
-  const nextTwo = [0,1]
-
-  for(let i = 0; i < n -1; ++i) {
-    nextTwo.push(nextTwo[nextTwo.length -1] + nextTwo[nextTwo.length-2])
-  }
-
-  return nextTwo[nextTwo.length -1]
+  // **iterative solution**
+  // const nextTwo = [0,1]
+  // for(let i = 0; i < n -1; ++i) {
+  //   nextTwo.push(nextTwo[nextTwo.length -1] + nextTwo[nextTwo.length-2])
+  // }
+  // return nextTwo[nextTwo.length -1]
 
 }
 
@@ -31,5 +32,10 @@ if (require.main === module) {
 
 module.exports = fibonacci;
 
+// When an arguement n is given, return the value in that position of the Fib Seq.
 // Basecases: If n < 2, return n. If nextTwo.length -1 = n, return nextTwo.length -1.
-// And a written explanation of your solution
+// Return value: the number in fib seq at position n.
+// Recursive function: n-1 (counter)
+// I learned you can return recursive calls, and perform operations using more than one call.
+// I also learned that the return value inside the recursive function breaks the loop whereas
+// the return value containing recursive functions creates the iteration.
